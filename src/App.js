117abbf94f80c422df3f8eae0505752bb0e28cf9
brunopenso <viperp@gmail.com>
2020-03-23 21:32:35 -0300

    base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Covid-19 Brasil - Acompanhamento do Gráfico e simulações
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="SmallText">
+          Para atualizar os dados deste gráfico é necessário fazer um update no arquivo dentro da pasta <b>data</b>
+        </p>
       </header>
     </div>
   );
